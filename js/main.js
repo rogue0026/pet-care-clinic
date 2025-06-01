e796a5852207d659aada2ae7f15d805ff614ed73
paul <rogue026@mail.ru>
2025-06-01 23:42:36 +0300

    Добавил модальное окно для записи на прием

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const enlistButton = document.querySelector('.enlist-button')
+enlistButton.addEventListener('click', evt => {
+    const modalWindow = document.querySelector('.modal')
+    modalWindow.classList.add('modal-container')
+})
+
+const closeModalWindobButton = document.querySelector('.modal-close-button')
+closeModalWindobButton.addEventListener('click', evt => {
+    const modalWindow = document.querySelector('.modal')
+    modalWindow.classList.remove('modal-container')
+})
